06978fef65f56ecdc915abd82a3760b2743230f7
Juan Vilchez <juanvilchez2002@gmail.com>
2021-03-03 20:04:45 -0500

    Let, const, multilinea y desestructuración - Terminado

--- src/es6/index.js
+++ src/es6/index.js
@@ -51,5 +51,29 @@ let {name, country} = person;
 console.log(name, country);
 
 //----------
-//operador de propagación
+//operador de propagación; unir dos arreglos en uno solo
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Jessica', 'Camila'];
 
+//unir con es6
+let education = ['David', ...team1, ...team2];
+console.log(education);
+
+//----------
+//var : contexto global
+{
+    var globalVar = 'Global Var';
+}
+//let : contexto del bloque de código
+{
+    let globalLet = 'Global Let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);//se ejecuta Var xq es global
+console.log(globalLet);//no se ejecuta Let x ser solo en bloque
+
+//---------
+//Const : es una constante, ambito de bloque y no global
+const a = 'b';
+a = 'a';
